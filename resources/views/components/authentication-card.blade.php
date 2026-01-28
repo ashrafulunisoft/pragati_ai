6a2ac284fcbed0d1ba83bf5e9dec7d8624da373a
ashrafulunisoft <ashrafulunisoft@gmail.com>
2026-01-28 10:18:58 +0000

    fix the all color: login , register, reset password

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,9 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0" style="background: linear-gradient(135deg, #0bd696 0%, #0d5540 100%);">
     <div>
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg" style="background: rgba(255, 255, 255, 0.95);">
         {{ $slot }}
     </div>
 </div>
